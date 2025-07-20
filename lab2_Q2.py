2730eba67ad8e63bd2ec083560137c84c4619442
Udhaya Sankari <udhayasankari046@gmail.com>
2025-07-20 23:23:38 +0530

    Auto-push new or updated Python file

--- lab2_Q2.py
+++ lab2_Q2.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-from sklearn.metrics import pairwise_distances, cosine_similarity
+from sklearn.metrics.pairwise import pairwise_distances, cosine_similarity
+
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 import seaborn as sns

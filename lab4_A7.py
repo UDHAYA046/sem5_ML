9461a66c0d98260493f8f3592782a72617255ec0
Udhaya Sankari <udhayasankari046@gmail.com>
2025-08-01 19:48:35 +0530

    Auto-push new or updated Python file

--- lab4_A7.py
+++ lab4_A7.py
@@ -9,10 +9,10 @@ from sklearn.neighbors import KNeighborsClassifier
 def fetch_filtered_dataset(csv_file):
     df = pd.read_csv(csv_file)
     filtered_df = df[df['class'].isin([1, 2])]
+    features = filtered_df[['mfcc1', 'pitch_std']].values
     labels = filtered_df['class'].values
-
-    labels = filtered_df['confidence_level'].values
     return features, labels
+
 
 #  Train-test split wrapper
 def partition_data(features, labels, test_ratio=0.3, seed=42):
